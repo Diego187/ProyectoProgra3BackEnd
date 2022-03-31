58b8d94089bd4ddac216d2bad55b7a8a22d688c9
Diego187 <dt18702@gmail.com>
2022-03-30 22:16:16 -0600

    Los demas archivos JAJA

---
+++ src/main/java/com/umg/ProyectoProgra3/ProyectoProgra3Application.java
@@ -0,0 +1,13 @@
+package com.umg.ProyectoProgra3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoProgra3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoProgra3Application.class, args);
+	}
+
+}
