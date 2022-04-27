8f3342d2c59d3c654592cef591748dabf0f86e89
ccaceresm1 <ccaceresm1@miumg.edu.gt>
2022-04-26 23:13:00 -0600

    servicio funcional de login

--- src/main/java/com/umg/ProyectoProgra3/service/LoginService.java
+++ src/main/java/com/umg/ProyectoProgra3/service/LoginService.java
@@ -1,6 +1,6 @@
 package com.umg.ProyectoProgra3.service;
 
-<<<<<<< HEAD
+
 import com.umg.ProyectoProgra3.entity.Login;
 import com.umg.ProyectoProgra3.entity.User;
 import com.umg.ProyectoProgra3.repository.UserRepository;
@@ -8,12 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-=======
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
->>>>>>> login
 
 @RestController
 @RequestMapping("/login")
@@ -23,9 +18,9 @@ public class LoginService {
     @Autowired
     UserRepository userRepository;
 
-
+ //Servicio que devuelve un objeto del user si lo encuentra si es en blanco o null es porque no pasa la validacion
     @PostMapping(path = "/user")
-    private List<User> loginTeacher(@RequestBody Login credentials){
+    private List<User>  loginTeacher(@RequestBody Login credentials){
         return userRepository.findByUserAndPassword (credentials.getUser(), credentials.getPass());
     }
 
