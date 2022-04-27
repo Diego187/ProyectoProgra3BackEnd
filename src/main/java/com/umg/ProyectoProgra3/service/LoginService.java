dcb9ed363bfd961c7b8cdf78a913f281c80144e8
ccaceresm1 <ccaceresm1@miumg.edu.gt>
2022-04-26 22:03:10 -0600

    cambios para login

--- src/main/java/com/umg/ProyectoProgra3/service/LoginService.java
+++ src/main/java/com/umg/ProyectoProgra3/service/LoginService.java
@@ -1,6 +1,21 @@
 package com.umg.ProyectoProgra3.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 public class LoginService {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    @PostMapping(path = "/login")
+    private Optional<User> loginTeacher(@RequestBody Login credentials){
+        return userRepository.findByUserAndPassword (credentials.getUser(), credentials.getPass());
+    }
+
 
 
 
