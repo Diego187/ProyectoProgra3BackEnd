31c69fd4f12d5c4bc27198268e7df414dac66451
ccaceresm1 <ccaceresm1@miumg.edu.gt>
2022-04-26 22:00:26 -0600

    cambios para login

--- src/main/java/com/umg/ProyectoProgra3/service/LoginService.java
+++ src/main/java/com/umg/ProyectoProgra3/service/LoginService.java
@@ -1,6 +1,18 @@
 package com.umg.ProyectoProgra3.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/login")
+@CrossOrigin
 public class LoginService {
+
+
+
+
 
 
 
