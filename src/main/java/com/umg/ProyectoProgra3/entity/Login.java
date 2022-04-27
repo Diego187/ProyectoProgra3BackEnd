e769e72851b8af68a969abb75f33892374ba1803
ccaceresm1 <ccaceresm1@miumg.edu.gt>
2022-04-26 21:41:32 -0600

    cambios proyecto nuevo

--- src/main/java/com/umg/ProyectoProgra3/entity/Login.java
+++ src/main/java/com/umg/ProyectoProgra3/entity/Login.java
@@ -1,39 +1,21 @@
 package com.umg.ProyectoProgra3.entity;
 
 
-import javax.persistence.*;
-import java.io.Serializable;
-
-@Entity
-@Table(name ="user")
-public class Login implements Serializable {
-
-    private static final long serialVersionUID = 3760988163877761705L;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
-
-    @Column(name = "iduser")
+public class Login  {
     private String user;
-
-    @Column(name = "password")
-    private String password;
-
+    private String pass;
 
     public String getUser() {
         return user;
     }
-
     public void setUser(String user) {
         this.user = user;
     }
-
-    public String getPassword() {
-        return password;
+    public String getPass() {
+        return pass;
+    }
+    public void setPass(String pass) {
+        this.pass = pass;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
