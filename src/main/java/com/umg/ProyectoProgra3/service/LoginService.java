624b0a388bb0e0d9e790e720553f33e6966aa372
ccaceresm1 <ccaceresm1@miumg.edu.gt>
2022-04-30 16:26:56 -0600

    cambio de login

--- src/main/java/com/umg/ProyectoProgra3/service/LoginService.java
+++ src/main/java/com/umg/ProyectoProgra3/service/LoginService.java
@@ -21,7 +21,8 @@ public class LoginService {
  //Servicio que devuelve un objeto del user si lo encuentra si es en blanco o null es porque no pasa la validacion
     @PostMapping(path = "/user")
     private List<User>  loginTeacher(@RequestBody Login credentials){
-
+        System.out.println("ingresa al servicio");
+        System.out.println(credentials);
         return userRepository.findByUserAndPassword (credentials.getUser(), credentials.getPass());
     }
 
