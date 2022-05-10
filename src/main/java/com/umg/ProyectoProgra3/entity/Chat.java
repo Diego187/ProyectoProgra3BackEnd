eb1d7135f8bab875f3cd2dcad8e77322f326f783
Diego187 <dt18702@gmail.com>
2022-05-09 22:44:41 -0600

    Desarrollo1 para logica de chats

---
+++ src/main/java/com/umg/ProyectoProgra3/entity/Chat.java
@@ -0,0 +1,33 @@
+package com.umg.ProyectoProgra3.entity;
+
+import java.util.ArrayList;
+
+public class Chat {
+    String name;
+    String description;
+    ArrayList<User> members ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(ArrayList<User> members) {
+        this.members = members;
+    }
+}
