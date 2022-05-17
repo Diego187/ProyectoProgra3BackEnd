04db34887c0e3573a438dfd5acdb9b99a40784b1
Diego187 <dt18702@gmail.com>
2022-05-16 21:22:19 -0600

    Creacion de chat y mensajes para desplegar dentro del canal

---
+++ src/main/java/com/umg/ProyectoProgra3/entity/Message.java
@@ -0,0 +1,40 @@
+package com.umg.ProyectoProgra3.entity;
+
+public class Message {
+    String date;
+    String time;
+    String user;
+    String message;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
