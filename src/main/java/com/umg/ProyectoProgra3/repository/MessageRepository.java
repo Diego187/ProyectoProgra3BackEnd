4d950636155b0f38af0910a49e41283a4aff2adb
ccaceresm1 <ccaceresm1@miumg.edu.gt>
2022-05-17 22:43:22 -0600

    cambios para pruebas de sql

--- src/main/java/com/umg/ProyectoProgra3/repository/MessageRepository.java
+++ src/main/java/com/umg/ProyectoProgra3/repository/MessageRepository.java
@@ -2,12 +2,9 @@ package com.umg.ProyectoProgra3.repository;
 
 
 import com.umg.ProyectoProgra3.entity.Message;
-import com.umg.ProyectoProgra3.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.io.Serializable;
-import java.util.List;
 
 @Repository("messageRepository")
 public interface MessageRepository  extends JpaRepository<Message, Serializable> {
