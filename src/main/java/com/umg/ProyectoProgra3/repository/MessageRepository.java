b062b5f4f62160ecbd20a94f289333321c7898f3
ccaceresm1 <ccaceresm1@miumg.edu.gt>
2022-05-17 22:36:17 -0600

    creacion de sql repository para pruebas de query

--- src/main/java/com/umg/ProyectoProgra3/repository/MessageRepository.java
+++ src/main/java/com/umg/ProyectoProgra3/repository/MessageRepository.java
@@ -1,12 +1,16 @@
 package com.umg.ProyectoProgra3.repository;
 
 
-import com.umg.ProyectoProgra3.entity.User;
+
+import com.umg.ProyectoProgra3.entity.Message;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
 @Repository("messageRepository")
 public interface MessageRepository {
-    public List<User> findByUserAndPassword(String user, String password);
+
+
+
 }
--- src/main/java/com/umg/ProyectoProgra3/repository/UserRepository.java
+++ src/main/java/com/umg/ProyectoProgra3/repository/UserRepository.java
@@ -2,16 +2,20 @@ package com.umg.ProyectoProgra3.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.umg.ProyectoProgra3.entity.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository("userRepository")
 public interface UserRepository extends JpaRepository<User, Serializable>{
     public List<User> findByUserAndPassword(String user, String password);
 
+    @Query(value = "select * from user  where idclient = ?1", nativeQuery = true)
+    Optional<User> findByUser(int idcliente) ;
 
 }
 
