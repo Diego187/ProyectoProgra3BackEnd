ce77555406005e1f0e97c2fe2c74c3146e9501e3
ccaceresm1 <ccaceresm1@miumg.edu.gt>
2022-06-04 16:58:59 -0600

    reactivo

---
+++ react/socketio-server/index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+
+//configuraciones
+app.set('port', process.env.PORT || 4201)
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname,'public')));
+
+//inicia server
+const server = app.listen(app.get('port'), ()=>{
+    console.log('server on port', app.get('port'));
+});
+
+//websockets
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection', (socket) => {
+    console.log('new conection', socket.id);
+
+    socket.on('chat:message', (data) => {
+       io.sockets.emit('chat:message', data);
+    });
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data);
+    });
+
+});
---
+++ react/socketio-server/public/chat.js
@@ -0,0 +1,35 @@
+const socket = io()
+
+
+//dom elements
+let message = document.getElementById('message');
+let username = document.getElementById('username');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+let actions = document.getElementById('actions');
+
+
+btn.addEventListener('click', function () {
+    socket.emit('chat:message', {
+        message: message.value,
+        username: username.value
+    });
+    message.value='';
+});
+
+    message.addEventListener('keypress', function () {
+        socket.emit('typing', username.value);
+    });
+
+    socket.on('chat:message', function  (data) {
+        actions.innerHTML ='';
+        output.innerHTML += `<p>
+        <strong>${data.username}</strong>: ${data.message}
+        </p>`
+    });
+
+    socket.on('typing', function (data) {
+        actions.innerHTML = `<p><em>${data} escribiendo...</em></p>`
+    });
+
+
